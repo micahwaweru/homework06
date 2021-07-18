8ced746b07f2d49a249ef3cde232eb57e0b3df0e
micahwaweru <micah.waweru@gmail.com>
2021-07-17 23:11:26 -0500

    saturday updates

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,22 @@
 console.log('Script attached');
-var baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
-var city = 'Chicago';
-var units = '&units=imperial';
-var APIKey = '&appid=e1a4a8808d7f6de7333f8ac6e7ef2b5d';
-var apiUrl= baseUrl + city + units + APIKey;
-console.log(apiUrl);
+
 var submitButton = document.querySelector('#submitButton');
 var searchTerm = document.querySelector('#searchTerm');
+var todayWeatherList = document.querySelector('#todayWeatherList');
+var heroHead = document.querySelector('#heroHead');
+var dateSub = document.querySelector('#dateSub');
 
 var searchButtonHandler = function(event){
     event.preventDefault();
     console.log('Button clicked!');
-    
+    var now = moment();
+    console.log(now.format('L'))
+    dateSub.textContent = now.format('L');
+
     if(searchTerm.value){
         console.log(searchTerm.value);
+        getWeather(searchTerm.value);
+        heroHead.textContent = searchTerm.value;
     }
     else{
         console.log('Please enter search term')
@@ -23,7 +26,15 @@ var searchButtonHandler = function(event){
 submitButton.addEventListener('click', searchButtonHandler);
 
 
-var getWeather = function(){
+var getWeather = function(city){
+  var baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  //var city = 'Chicago';
+  var units = '&units=imperial';
+  var daily = '&daily.temp.day'
+  var APIKey = '&appid=e1a4a8808d7f6de7333f8ac6e7ef2b5d';
+  var apiUrl= baseUrl + city + units + daily + APIKey;
+  console.log(apiUrl);
+
     fetch(apiUrl)
     .then(
       function(response) {
@@ -42,6 +53,20 @@ var getWeather = function(){
           console.log('Temp: ' + data.main.temp);
           console.log('Humidity: ' + data.main.humidity + '%')
           console.log('Wind: ' + wind + 'MPH');
+
+          var tempItem = document.createElement('li');
+			    var humidityItem = document.createElement('li');
+			    var windItem = document.createElement('li');
+
+			    tempItem.textContent = 'Temp: ' + temperature;
+			    humidityItem.textContent = 'Humidity: ' + humidity;
+			    windItem.textContent  = 'Wind: ' + wind + 'MPH';
+
+          todayWeatherList.appendChild(tempItem);
+          todayWeatherList.appendChild(humidityItem);
+          todayWeatherList.appendChild(windItem);
+
+
         });
       }
     )
@@ -51,4 +76,4 @@ var getWeather = function(){
 
 }
 
-getWeather();
+//getWeather();
